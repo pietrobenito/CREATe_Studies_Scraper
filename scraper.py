af9ef4c9854366296c6a39198393e0cbeaa9997f
pietrobenito <petebennett1982@gmail.com>
2016-08-09 14:34:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,12 +7,11 @@ import lxml.html
 # # Read in a page
 html = scraperwiki.scrape("http://papers.ssrn.com/sol3/DisplayJournalBrowse.cfm")
 
-print html
-
-
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+root.cssselect("a[class='journal_label']")
+
+print root
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
