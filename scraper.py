6b4a235b6baba80e0534513bb9d910e491840dd9
pietrobenito <petebennett1982@gmail.com>
2016-08-21 18:32:01 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,9 @@ html = scraperwiki.scrape("http://www.ssrn.com/link/Intellectual-Property-Copyri
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 for linkText in root.cssselect("a[class='textlink']"):
-  scraperwiki.sqlite.save(unique_keys=['url'], data={'title' : linkText.text_content(), 'url' : linkText.get('href')})
+  data={'title' : linkText.text_content(), 'url' : linkText.get('href')}
+
+scraperwiki.sqlite.save(unique_keys=['url'], data=data)
 
 print scraperwiki.sqlite.show_tables()
 #
