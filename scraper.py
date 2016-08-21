ade4129aa51d98afd98640f7c09aab0b726986d8
pietrobenito <petebennett1982@gmail.com>
2016-08-21 20:00:43 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,30 +1,21 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-import scraperwiki,lxml.html,sqlite3,urllib
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-html = scraperwiki.scrape("http://www.ssrn.com/link/Intellectual-Property-Copyright-Law.html")
-
+html = scraperwiki.scrape("http://papers.ssrn.com/sol3/DisplayJournalBrowse.cfm")
+#
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-data = []
-for linkText in root.cssselect("a[class='textlink']"):
-  data.append({"title" : linkText.text_content().encode('utf-8'), "url" : linkText.get('href').encode('utf-8')})
-
-for row in data:
-  print row['title'].decode("ascii", "ignore")
-#  scraperwiki.sqlite.save(unique_keys=['url'], data={"title" : row['title'].decode("ascii", "ignore"), "url" : row['url'].decode("ascii", "ignore")})
-#  scraperwiki.sqlite.save(unique_keys=['url'], data={"title" : "test123", "url" : "test456"})
-#scraperwiki.sqlite.save(unique_keys=['url'], data=data)
-#scraperwiki.sqlite.save(unique_keys=['url'], data={"title":"test123","url":"test456"})
-scraperwiki.sqlite.save(unique_keys=['a'], data={"a":1, "bbb":"Hi there"})
-
-
-print scraperwiki.sqlite.show_tables()
+root.cssselect("div[class='node']")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['a'], data={"a":1, "bbb":"Hi there"})
+
+print scraperwiki.sqlite.show_tables()
+
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
@@ -34,3 +25,8 @@ print scraperwiki.sqlite.show_tables()
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+
+
+
+
